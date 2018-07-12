6085d8179fc9bc2e2dc4b7284281111b19e5fc51
Lonami Exo <totufals@hotmail.com>
2018-07-13 01:24:07 +0200

    Add arguments to support editing the bot

--- fathercli.py
+++ fathercli.py
@@ -107,15 +107,23 @@ async def load_bots(client):
     return bots
 
 
-async def get_bot_menu(client, bot_id):
+async def get_bot_menu(client, bot_id, subpart=None):
     async with aclosing(iter_buttons(client)) as it:
         async for button in it:
             if get_bot_id(button) == bot_id:
-                return await await_event(
+                message = await await_event(
                     client,
                     events.MessageEdited(FATHER),
                     pre=button.click()
                 )
+                if subpart:
+                    message = await await_event(
+                        client,
+                        events.MessageEdited,
+                        pre=message.click(data=button.data + b'/' + subpart)
+                    )
+
+                return message
         else:
             eprint('No bot with ID', bot_id, 'found')
 
@@ -199,6 +207,27 @@ async def create_bot(client, config, name):
     eprint('Bot created but failed to retrieve token')
 
 
+async def edit(client, bot_id, name, action):
+    message = await get_bot_menu(client, bot_id, b'edit')
+    path = 'bots/{}/edit/{}'.format(bot_id, name).encode('ascii')
+    await await_event(
+        client,
+        events.MessageEdited(FATHER),
+        pre=message.click(data=path)
+    )
+    await action
+
+
+async def edit_commands(client, bot_id, commands):
+    for i, cmd in enumerate(commands):
+        cmd = cmd.split('-', 1)
+        l, r = cmd if len(cmd) == 2 else (cmd, None)
+        commands[i] = '{} - {}'.format(l, r or '(no description)')
+
+    await edit(client, bot_id, 'comm',
+               client.send_message(FATHER, '\n'.join(commands)))
+
+
 async def main():
     config = Config()
     parser = argparse.ArgumentParser()
@@ -217,7 +246,7 @@ async def main():
     no_bot('-l', '--list', action='store_true',
            help='lists owned bots (reloads if no bots are known)')
 
-    need_bot('-n', '--newtoken', metavar='BOT',
+    need_bot('-g', '--gentoken', metavar='BOT',
              help='generate and get a new token for a bot')
 
     need_bot('-t', '--token', metavar='BOT',
@@ -225,6 +254,24 @@ async def main():
 
     need_bot('-d', '--delete', metavar='BOT',
              help='deletes a bot')
+
+    need_bot('-n', '--name', nargs=2, metavar=('BOT', 'NAME'),
+             help='set a new name for a bot')
+
+    need_bot('-i', '--info', nargs=2, metavar=('BOT', 'INFO'),
+             help='set the info for the bot shown in the chat')
+
+    need_bot('-b', '--bio', nargs=2, metavar=('BOT', 'BIO'),
+             help='set the biography (about) for the bot')
+
+    need_bot('-p', '--photo', nargs=2, metavar=('BOT', 'PHOTO'),
+             help='set the profile photo for a bot')
+
+    need_bot('-m', '--commands', nargs='+', metavar=('BOT', 'CMD-DESC'),
+             help='set the commands with their description for the bot')
+
+    need_bot('-e', '--inline', nargs=2, metavar=('BOT', 'HINT'),
+             help='set the inline placeholder hint')
 
     args = parser.parse_args()
     if not config.api_id and not args.api:
@@ -251,13 +298,45 @@ async def main():
         if args.create:
             print(await create_bot(client, config, args.create))
 
-        if args.token or args.newtoken:
-            bot_id = find_bot(config, args.token or args.newtoken)
-            print(await get_token(client, bot_id, revoke=bool(args.newtoken)))
+        if args.token or args.gentoken:
+            bot_id = find_bot(config, args.token or args.gentoken)
+            print(await get_token(client, bot_id, revoke=bool(args.gentoken)))
 
         if args.delete:
             bot_id = find_bot(config, args.delete)
             await delete_bot(client, config, bot_id)
+
+        if args.name:
+            bot_id = find_bot(config, args.name[0])
+            await edit(client, bot_id, 'name',
+                       client.send_message(FATHER, args.name[1]))
+
+        if args.info:
+            bot_id = find_bot(config, args.info[0])
+            await edit(client, bot_id, 'desc',
+                       client.send_message(FATHER, args.info[1]))
+
+        if args.bio:
+            bot_id = find_bot(config, args.bio[0])
+            await edit(client, bot_id, 'desc',
+                       client.send_message(FATHER, args.bio[1]))
+
+        if args.photo:
+            bot_id = find_bot(config, args.photo[0])
+            await edit(client, bot_id, 'pic',
+                       client.send_file(FATHER, args.photo[1]))
+
+        if args.commands:
+            commands = args.commands[1:]
+            if commands:
+                bot_id = find_bot(config, args.commands[0])
+                await edit_commands(client, bot_id, commands)
+
+        if args.inline:
+            bot_id = find_bot(config, args.inline[0])
+            await edit(client, bot_id, 'inph',
+                       client.send_file(FATHER, args.inline[1]))
+
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
